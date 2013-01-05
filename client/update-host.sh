d68bfc97057d8ece95d0ff5bfd16f867bc7069a0
e3rpay <e3rpay@gmail.com>
2013-01-05 15:58:41 +0000

    syntax error

--- client/update-host.sh
+++ client/update-host.sh
@@ -3,6 +3,6 @@ domain="host.me"
 addr=$(cat ../server/addr)
 if [[ $(grep "^.*$domain" /etc/hosts) ]]; then
 sudo sed -i "s/^.*$domain/$addr\t$domain/g" /etc/hosts
-else;
+else
 echo -e "$addr\t$domain" | sudo tee -a /etc/hosts
 fi
