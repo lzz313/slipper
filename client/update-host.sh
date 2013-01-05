ae8916972b40c6dd1e0987c53b84e4b72828a552
Peer Xu <pppeerxu@gmail.com>
2013-01-06 00:03:00 +0800

    get ip address from github

--- client/update-host.sh
+++ client/update-host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 domain="host.me"
-addr=$(cat ../server/addr)
+addr=$(curl https://raw.github.com/e3rpay/slipper/master/server/addr)
 if [[ $(grep "^.*$domain" /etc/hosts) ]]; then
 sudo sed -i "s/^.*$domain/$addr\t$domain/g" /etc/hosts
 else
