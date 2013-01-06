00d931c8b9436b7af3f7c3dadaa2c636d9ec67b4
Peer Xu <pppeerxu@gmail.com>
2013-01-06 20:55:44 +0800

    hahaha

--- client/update-host.sh
+++ client/update-host.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+username="e3rpay"
 domain="host.me"
-addr=$(curl https://raw.github.com/e3rpay/slipper/master/server/addr)
+addr=$(curl https://raw.github.com/$username/slipper/master/server/addr)
 if [[ $(grep "^.*$domain" /etc/hosts) ]]; then
 sudo sed -i "s/^.*$domain/$addr\t$domain/g" /etc/hosts
 else
--- server/update.sh
+++ server/update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-./lookup.sh
-./post.sh
+SLIPPER_HOME=/home/$(whoami)/.slipper/server
+
+cd $SLIPPER_HOME
+
+echo $(curl ifconfig.me) > addr
+git add .
+git commit -m "commit by post.sh on $(date)" -a
+git push
