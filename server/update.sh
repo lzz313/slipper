b4443c2e243ab7600a3c46c64eaa33b88d0e2e8f
Peer Xu <pppeerxu@gmail.com>
2013-01-10 22:52:50 +0800

    tag 1.0

--- server/update.sh
+++ server/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SLIPPER_HOME=/home/$(whoami)/.slipper/server
+SLIPPER_HOME=/home/$(whoami)/.slipper
 
 cd $SLIPPER_HOME
 
-echo $(curl ifconfig.me) > addr
+echo $(curl ifconfig.me) > index.html
 git add .
 git commit -m "commit by post.sh on $(date)" -a
 git push
