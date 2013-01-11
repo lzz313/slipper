eff600101d104ee222a46d5c6cd8a18b519863e3
Peer Xu <pppeerxu@gmail.com>
2013-01-11 15:04:35 +0800

    a big upgrade

--- server/update.sh
+++ server/update.sh
@@ -4,7 +4,15 @@ SLIPPER_HOME=/home/$(whoami)/.slipper
 
 cd $SLIPPER_HOME
 
-echo $(curl ifconfig.me) > index.html
+address=$(curl ifconfig.me)
+
+cat << EOF > addr.js
+window.slipper = {
+    "addr": "$address"
+}
+EOF
+echo $address > addr
+
 git add .
 git commit -m "commit by post.sh on $(date)" -a
 git push
