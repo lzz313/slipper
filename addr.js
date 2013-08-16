46a7590587f11de9697acc6f1c73c4ddb8cf68db
lzz313 <lzz313@sina.com>
2013-08-16 13:07:34 +0800

    commit by post.sh on Fri Aug 16 13:07:34 CST 2013

--- addr.js
+++ addr.js
@@ -1,9 +1,1 @@
-{"addr": "58.209.216.30"} EOF
-echo 58.209.216.30 > addr
-
-git pull origin gh-pages
-git add .
-git commit -m "commit by post.sh on Fri Aug 16 13:03:23 CST 2013" -a
-git push origin gh-pages
 {addr: 58.209.216.30}
-{"addr": "$address"}
